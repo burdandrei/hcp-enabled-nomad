ac6d0c46a20b568ef02e1f211e17483aebcb3c7a
Andrei Burd <burdandrei@users.noreply.github.com>
2021-06-01 15:37:10 +0300

    public and tier

--- 02-hvn/vault-consul.tf
+++ 02-hvn/vault-consul.tf
@@ -1,9 +1,12 @@
 resource "hcp_vault_cluster" "demo_hcp_vault" {
-  hvn_id     = hcp_hvn.demo_hcp_hvn.hvn_id
-  cluster_id = "demo-vault"
+  hvn_id          = hcp_hvn.demo_hcp_hvn.hvn_id
+  cluster_id      = "demo-vault"
+  public_endpoint = true
 }
 
 resource "hcp_consul_cluster" "demo_hcp_consul" {
-  hvn_id     = hcp_hvn.demo_hcp_hvn.hvn_id
-  cluster_id = "demo-consul"
+  hvn_id          = hcp_hvn.demo_hcp_hvn.hvn_id
+  cluster_id      = "demo-consul"
+  tier            = "development"
+  public_endpoint = true
 }
