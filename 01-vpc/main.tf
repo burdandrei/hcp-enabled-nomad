dbef69030bfbcad4953691bf02b4477246134722
Andrei Burd <burdandrei@users.noreply.github.com>
2021-06-01 13:15:22 +0300

    vpc add

---
+++ 01-vpc/main.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name   = var.name
+  cidr   = var.cidr
+
+  azs            = slice(data.aws_availability_zones.available.*.names[0], 0, 3)
+  public_subnets = var.public_subnets
+  public_subnet_tags = {
+    Name = "${var.name}-public"
+  }
+  tags = {
+    Terraform = "true"
+    Name      = "hc_demo"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
---
+++ 01-vpc/provider.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.42.0"
+    }
+  }
+}
+
+provider "aws" {}
