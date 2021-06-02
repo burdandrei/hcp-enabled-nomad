f58cfc8e06fd8f272d1aabb31a26361aacc39469
Andrei Burd <burdandrei@users.noreply.github.com>
2021-06-02 09:45:20 +0300

    peer to right VPC

--- 01-vpc/outputs.tf
+++ 01-vpc/outputs.tf
@@ -1,3 +1,23 @@
 output "cidr_block" {
   value = module.vpc.vpc_cidr_block
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "public_route_table_ids" {
+  value = module.vpc.public_route_table_ids
+}
+
+output "owner_id" {
+  value = module.vpc.owner_id
+}
+
+output "region" {
+  value = module.vpc.region
+}
--- 02-hvn/hvn.tf
+++ 02-hvn/hvn.tf
@@ -18,23 +18,23 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
-resource "aws_vpc" "peer" {
-  cidr_block = data.terraform_remote_state.vpc.outputs.cidr_block
-}
-
-data "aws_arn" "peer" {
-  arn = aws_vpc.peer.arn
-}
 
 resource "hcp_aws_network_peering" "peer" {
   hvn_id              = hcp_hvn.demo_hcp_hvn.hvn_id
-  peer_vpc_id         = aws_vpc.peer.id
-  peer_account_id     = aws_vpc.peer.owner_id
-  peer_vpc_region     = data.aws_arn.peer.region
-  peer_vpc_cidr_block = aws_vpc.peer.cidr_block
+  peer_vpc_id         = data.terraform_remote_state.vpc.outputs.vpc_id
+  peer_account_id     = data.terraform_remote_state.vpc.outputs.owner_id
+  peer_vpc_region     = data.terraform_remote_state.vpc.outputs.region
+  peer_vpc_cidr_block = data.terraform_remote_state.vpc.outputs.cidr_block
 }
 
 resource "aws_vpc_peering_connection_accepter" "peer" {
   vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
   auto_accept               = true
+}
+
+
+resource "aws_route" "hvn-peering" {
+  route_table_id            = data.terraform_remote_state.vpc.outputs.public_route_table_ids[0]
+  destination_cidr_block    = hcp_hvn.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.peer.id
 }
